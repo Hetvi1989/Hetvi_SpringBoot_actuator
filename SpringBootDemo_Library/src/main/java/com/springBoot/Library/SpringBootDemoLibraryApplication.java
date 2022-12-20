99e402bc20471f04cf52f7edafb05c0055f69cd0
Hetvi1989 <110095347+Hetvi1989@users.noreply.github.com>
2022-12-20 14:58:19 +0530

    Add files via upload

---
+++ SpringBootDemo_Library/src/main/java/com/springBoot/Library/SpringBootDemoLibraryApplication.java
@@ -0,0 +1,16 @@
+ package com.springBoot.Library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoLibraryApplication.class, args);
+		System.out.println("Hello devtools");
+		System.out.println("Hi... all");
+		System.out.println("This is Spring Boot demo	");
+	}
+
+}
---
+++ SpringBootDemo_Library/src/test/java/com/springBoot/Library/SpringBootDemoLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBoot.Library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
